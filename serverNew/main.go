f589f30a9714f82fff3dfc1534f2e0ce3e4e373d
Mateusz <mati1986@gmail.com>
2016-01-23 16:54:51 +0100

    port serwera

--- serverNew/main.go
+++ serverNew/main.go
@@ -20,15 +20,17 @@ func sendResponse(conn *net.UDPConn, addr *net.UDPAddr, msg string) {
 func main() {
 	log.SetFlags(log.Lshortfile)
 
-	addr := net.UDPAddr{
-		Port: 12888,
-		IP:   net.ParseIP("127.0.0.1"),
-	}
-	ser, err := net.ListenUDP("udp", &addr)
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":8081")
 	if err != nil {
 		fmt.Printf("Some error %v\n", err)
 		return
 	}
+	ser, err := net.ListenUDP("udp", ServerAddr)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return
+	}
+
 	server := engine.NewServer(ser)
 	go server.Listen()
 	log.Println("listening on 12888...")
@@ -43,12 +45,12 @@ func main() {
 		msg := make([]byte, 100)
 		n, remoteaddr, err := ser.ReadFromUDP(msg)
 		fmt.Printf("Read a message from %v %s \n", remoteaddr, msg)
+
 		if err != nil {
 			fmt.Printf("Some error  %v", err)
 			continue
 		}
 		tmp := strings.Split(string(msg[:n]), ":")
-		//		log.Println(string(msg))
 		switch string(tmp[0]) {
 		case "login":
 			client := server.NewClient(remoteaddr)
